d01638defa573707473f7e876992c9726e0b19dd
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-03 15:21:24 -0700

    feature: Add merchant bulk discount delete

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -14,15 +14,23 @@ class BulkDiscountsController < ApplicationController
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.new(bulk_discount_params)
     if params[:bulk_discount][:quantity].blank? || params[:bulk_discount][:discount].blank?
       flash[:notice] = "One or more of the required fields is empty. Bulk Discount cannot be Created!"
-      redirect_to new_merchant_bulk_discount_path(@merchant.id)
-    elsif @discount.save
-      flash[:notice] = "Discount##{@discount.id} has been Created!"
-      redirect_to merchant_bulk_discounts_path(@merchant.id)
+      redirect_to new_merchant_bulk_discount_path(merchant.id)
+    elsif discount.save
+      flash[:notice] = "Discount##{discount.id} has been Created!"
+      redirect_to merchant_bulk_discounts_path(merchant.id)
     end
+  end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.find(params[:id])
+    discount.destroy
+    flash[:notice] = "Discount ##{discount.id} has been Deleted!"
+    redirect_to merchant_bulk_discounts_path(merchant.id)
   end
 
 private
