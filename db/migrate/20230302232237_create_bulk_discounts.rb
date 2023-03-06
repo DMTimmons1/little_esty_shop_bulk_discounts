fe613d34abfa538279a2e931d3f2998e3e127f8e
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-05 19:00:21 -0700

    feature: discounted revenue for merchant invoice show page

--- db/migrate/20230302232237_create_bulk_discounts.rb
+++ db/migrate/20230302232237_create_bulk_discounts.rb
@@ -2,7 +2,7 @@ class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bulk_discounts do |t|
       t.integer :quantity
-      t.integer :discount
+      t.float :discount
       t.references :merchant, foreign_key: true
 
       t.timestamps
