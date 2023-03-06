653fb4d0517000c44eb3a5475cc0b27a986e4a12
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-05 18:59:14 -0700

    Add discounted revenue method and test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,14 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def discounted_revenue
+    invoice_totals = invoice_items.joins(item: [merchant: :bulk_discounts])
+    .select('sum(CASE WHEN invoice_items.quantity >= bulk_discounts.quantity 
+            THEN (invoice_items.unit_price * invoice_items.quantity) - ((invoice_items.unit_price * invoice_items.quantity) * (bulk_discounts.discount / 100)) 
+            ELSE invoice_items.unit_price * invoice_items.quantity END) as total_price')
+    .group(:id)
+    
+    Invoice.from(invoice_totals).sum('total_price')
+  end
 end
